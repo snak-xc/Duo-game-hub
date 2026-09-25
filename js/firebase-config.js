a4b0721196568680592717d198f6cb3f1827a233
snak-xc <ashenmadusanka61@gmail.com>
2026-09-25 20:18:51 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -2,7 +2,7 @@
 export const firebaseConfig = {
   apiKey: "AIzaSyAJDshREy6iYlsFHoBB6QxMOHGg6Drh-Fc",
   authDomain: "duo-game-hub-88.firebaseapp.com",
-  databaseURL: "https://PASTE_PROJECT_ID-default-rtdb.firebaseio.com",
+  databaseURL: "https://duo-game-hub-88-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "duo-game-hub-88",
   storageBucket: "duo-game-hub-88.firebasestorage.app",
   messagingSenderId: "573278889187",
