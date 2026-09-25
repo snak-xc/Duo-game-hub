f9d30728ecb22d14008c5bb870704e6ce9572514
snak-xc <ashenmadusanka61@gmail.com>
2026-09-25 18:44:02 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,10 @@
 // Replace ONLY the values below with the config from Firebase Console > Project settings > Your apps.
 export const firebaseConfig = {
-  apiKey: "PASTE_API_KEY",
-  authDomain: "PASTE_PROJECT_ID.firebaseapp.com",
+  apiKey: "AIzaSyAJDshREy6iYlsFHoBB6QxMOHGg6Drh-Fc",
+  authDomain: "duo-game-hub-88.firebaseapp.com",
   databaseURL: "https://PASTE_PROJECT_ID-default-rtdb.firebaseio.com",
-  projectId: "PASTE_PROJECT_ID",
-  storageBucket: "PASTE_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "PASTE_MESSAGING_SENDER_ID",
-  appId: "PASTE_APP_ID"
+  projectId: "duo-game-hub-88",
+  storageBucket: "duo-game-hub-88.firebasestorage.app",
+  messagingSenderId: "573278889187",
+  appId: "1:573278889187:web:18e4a4e4dd6c6c4c0fd1d6"
 };
