182cde1ab09eea9e364cf98bd039f335c09b776d
snak-xc <ashenmadusanka61@gmail.com>
2026-09-25 17:43:29 +0530

    Add files via upload

---
+++ js/firebase-config.js
@@ -0,0 +1,10 @@
+// Replace ONLY the values below with the config from Firebase Console > Project settings > Your apps.
+export const firebaseConfig = {
+  apiKey: "PASTE_API_KEY",
+  authDomain: "PASTE_PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://PASTE_PROJECT_ID-default-rtdb.firebaseio.com",
+  projectId: "PASTE_PROJECT_ID",
+  storageBucket: "PASTE_PROJECT_ID.firebasestorage.app",
+  messagingSenderId: "PASTE_MESSAGING_SENDER_ID",
+  appId: "PASTE_APP_ID"
+};
